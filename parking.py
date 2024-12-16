c964c834a90f707e05632c26c9a546f126c4cbc8
Hsu, Yao-Chih <a102193c0717@gmail.com>
2024-12-16 19:18:25 +0800

    add new python file named 'event'

--- parking.py
+++ parking.py
@@ -1,6 +1,6 @@
 """
 Information:
-    Title = 'Parking model for the parking lot in Zhixue station'
+    Title = 'Main program for parking model for the parking lot in Zhixue station'
     Author = 'Hsu, Yao-Chih, Xie, Yi-Xuan, Sin, Wen-Lee'
     Version = [1131216]
     Reference = ['Class of Simulation Study by C. Wang at 2024 fall']
@@ -10,31 +10,7 @@ Information:
 import random
 import time
 import numpy as np
-from scipy.stats import t
-
-### function
-def bicycle_parked_in_motorcycle_space(new_bicycle, bicycle_parked, remain_motorcycle_parking_space):
-    if new_bicycle == 0:
-        return new_bicycle, bicycle_parked, remain_motorcycle_parking_space
-    elif new_bicycle % 2 == 0:
-        space = new_bicycle / 2
-        new_bicycle = 0
-    else:
-        space = int(new_bicycle / 2) + 1
-        new_bicycle = 1
-
-    if bicycle_parked % 2 == 1 and new_bicycle == 1:
-        space -= 1
-        new_bicycle = 0
-
-    if space <= remain_motorcycle_parking_space:
-        remain_motorcycle_parking_space += space
-        bicycle_cannot_park = 0
-    else:
-        bicycle_cannot_park = 2 * (space - remain_motorcycle_parking_space) - new_bicycle
-        remain_motorcycle_parking_space = 0
-        new_bicycle = 0
-    return new_bicycle, bicycle_parked, remain_motorcycle_parking_space, bicycle_cannot_park
+from event import *
 
 ### main program
 # initial value
