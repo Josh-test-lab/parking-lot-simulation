1a125d22cb3e8d81e79cf158475d9f3d2a62b4e4
Hsu, Yao-Chih <a102193c0717@gmail.com>
2024-12-16 19:39:40 +0800

    update

--- parking.py
+++ parking.py
@@ -16,11 +16,17 @@ from event import *
 # initial value
 t = 0
 max_simulation_time = 10 * 365 * 24
-remain_car_parking_space = 627
-remain_motorcycle_parking_space = 90
+
+# the parking spaces
+remain_car_parking_space = 90
+remain_motorcycle_parking_space = 627
+
+# number of spaces which be parked
 car_parked = 0
 motorcycle_parked = 0
 bicycle_parked = 0
+
+# number of new vehicle 
 new_car = 0
 new_motorcycle = 0
 new_bicycle = 0
