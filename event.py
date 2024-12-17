9825e25271dfd9fdc937fafd55b5eb32d739b136
Hsu, Yao-Chih <a102193c0717@gmail.com>
2024-12-17 23:31:05 +0800

    bug fixed again

--- event.py
+++ event.py
@@ -76,7 +76,8 @@ def bicycle_leave_event():
 
 def motorcycle_leave_event(new_motorcycle, motorcycle_parked, remain_motorcycle_parking_space, max_motorcycle_parking_space):
     if new_motorcycle == 0:
-        return motorcycle_parked, remain_motorcycle_parking_space, 0
+        motorcycle_leave_failed = 0
+        return motorcycle_parked, remain_motorcycle_parking_space, motorcycle_leave_failed
     
     if motorcycle_parked < new_motorcycle:
         motorcycle_leave_failed = new_motorcycle - motorcycle_parked
