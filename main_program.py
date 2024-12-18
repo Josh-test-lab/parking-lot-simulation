37af73bce25a358e3bc267635a6acdc84b0b3298
Hsu, Yao-Chih <a102193c0717@gmail.com>
2024-12-18 09:54:07 +0800

    bug fixed

--- main_program.py
+++ main_program.py
@@ -20,17 +20,22 @@ max_simulation_time = 10 * 365 * 24 # total time
 
 # the parking spaces
 max_car_parking_space = 90
-max_motorcycle_parking_space = 627
+max_motorcycle_parking_space = 7
 remain_car_parking_space = max_car_parking_space
 remain_motorcycle_parking_space = max_motorcycle_parking_space
 
 # number of spaces which be parked now
 car_parked = 0
-motorcycle_parked = 0
+motorcycle_parked = 2
 bicycle_parked = 0
 
 # number of new vehicle enter/leave the parking lot
 new_car = 0
 new_motorcycle = 0
-new_bicycle = 0
+new_bicycle = 1
+
+bicycle_parked, remain_motorcycle_parking_space, bicycle_cannot_park = bicycle_parked_in_motorcycle_space_event(new_bicycle, bicycle_parked, remain_motorcycle_parking_space):
+[bicycle_parked, remain_motorcycle_parking_space, bicycle_cannot_park]
+
+
 
