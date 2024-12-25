a658575150165e9db32917f83cc2e862cff6df91
Hsu, Yao-Chih <a102193c0717@gmail.com>
2024-12-25 12:17:46 +0800

    update

--- function.py
+++ function.py
@@ -111,6 +111,9 @@ def parking_simulate(path_to_initial_value_json_file):
         car_parked, remain_car_parking_space, car_cannot_park = vehicle_parked_event(new_car, car_parked, remain_car_parking_space)
         motorcycle_cannot_park, bicycle_cannot_park = 0, 0
 
+        # debug
+        print(f't = {t}, clock = {clock}, new_car: {new_car}, new_motorcycle: {new_motorcycle}, new_bicycle: {new_bicycle}, walker: {int(np.sum(passengers) - np.sum(new_vehicles))}')
+
         while new_motorcycle >= 0 and new_bicycle >= 0:
             if random.random() < 0.5:
                 motorcycle_parked, remain_motorcycle_parking_space, motorcycle_cannot_park_counter = vehicle_parked_event(1, motorcycle_parked, remain_motorcycle_parking_space)
@@ -146,12 +149,9 @@ def parking_simulate(path_to_initial_value_json_file):
         passengers_list.append([passengers[0], passengers[1], new_vehicles[0][0], new_vehicles[0][1], new_vehicles[1][0], new_vehicles[1][1], new_vehicles[2][0], new_vehicles[2][1], int(np.sum(passengers) - np.sum(new_vehicles))])
         clocks.append([t, clock])
 
-        # debug
-        print(f't = {t}, clock = {clock}, new_car: {new_car}, new_motorcycle: {new_motorcycle}, new_bicycle: {new_bicycle}, walker: {int(np.sum(passengers) - np.sum(new_vehicles))}')
-
         # update time variables
         if clock == 23:
-            clock == 0
+            clock = 0
         else:
             clock += 1
         t += 1
