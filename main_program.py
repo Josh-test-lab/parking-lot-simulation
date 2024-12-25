bbbc0d13582fc3bb0eed3d5672c6a05881b818b6
Hsu, Yao-Chih <a102193c0717@gmail.com>
2024-12-25 12:59:15 +0800

    update main program

--- main_program.py
+++ main_program.py
@@ -10,15 +10,33 @@ Note: This is the main code of simulation. For simulation of different scenarios
 ### import module
 from event import *
 from function import *
+import matplotlib.pyplot as plt
 
 ### main program
 
 ## scenario 1
 # initial value file path
-path_to_initial_value_json_file = r'scenario\scenario_1\initial_values_for_scenario_1.json'
+path_to_initial_value_json_file = r'scenario\scenario_1\initial_values_for_scenario_1 copy.json'
 # simulate the parking lot
 result = parking_simulate(path_to_initial_value_json_file)
 print(f'CPU time is {result[5]} seconds.')
 
+i = 3
+for hour in range(87600):
+    print(f'car_parked: {result[i][hour][0]}, motorcycle_parked: {result[i][hour][1]}, bicycle_parked: {result[i][hour][2]}')
 
+a = []
+for hour in range(24,48):
+    a.append(result[1][hour][2] + result[1][hour][3])
+x = range(24)
+plt.figure(figsize=(10, 5))
+plt.plot(x, a, marker='o', label='Data Points')
+plt.title('Plot of Data Points')
+plt.xlabel('Hour (0 to 23)')
+plt.ylabel('Values')
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.xticks(x)  # Ensure x-axis ticks match the data
+plt.legend()
+plt.tight_layout()
+plt.show()
 
