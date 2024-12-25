d2ba7f9484f1cbe054b3c3a24340aa26223076ee
Hsu, Yao-Chih <a102193c0717@gmail.com>
2024-12-25 13:22:22 +0800

    update

--- main_program.py
+++ main_program.py
@@ -40,3 +40,64 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
+
+
+
+## scenario 2
+# initial value file path
+path_to_initial_value_json_file = r'scenario\scenario_2\initial_values_for_scenario_2.json'
+# simulate the parking lot
+result = parking_simulate(path_to_initial_value_json_file)
+print(f'CPU time is {result[5]} seconds.')
+
+i = 3
+for hour in range(87600):
+    print(f'car_parked: {result[2][hour][0]}, motorcycle_parked: {result[i][hour][1]}, bicycle_parked: {result[i][hour][2]}')
+
+a = []
+for hour in range(24,48):
+    a.append(result[1][hour][2] + result[1][hour][3])
+x = range(24)
+plt.figure(figsize=(10, 5))
+plt.plot(x, a, marker='o', label='Data Points')
+plt.title('Plot of Data Points')
+plt.xlabel('Hour (0 to 23)')
+plt.ylabel('Values')
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.xticks(x)  # Ensure x-axis ticks match the data
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+
+
+## scenario 3
+# initial value file path
+path_to_initial_value_json_file = r'scenario\scenario_3\initial_values_for_scenario_3.json'
+# simulate the parking lot
+result = parking_simulate(path_to_initial_value_json_file)
+print(f'CPU time is {result[5]} seconds.')
+
+i = 3
+for hour in range(87600):
+    print(f'car_parked: {result[2][hour][0]}, motorcycle_parked: {result[i][hour][1]}, bicycle_parked: {result[i][hour][2]}')
+
+a = []
+for hour in range(24,48):
+    a.append(result[1][hour][2] + result[1][hour][3])
+x = range(24)
+plt.figure(figsize=(10, 5))
+plt.plot(x, a, marker='o', label='Data Points')
+plt.title('Plot of Data Points')
+plt.xlabel('Hour (0 to 23)')
+plt.ylabel('Values')
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.xticks(x)  # Ensure x-axis ticks match the data
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+
+
